b73c7729225c290a01768cbaa55465dd2eee30cf
Estellanini <1357436238@qq.com>
2019-12-07 17:45:32 +0800

    完成到523，未写showWeatherInfo函数

--- app/src/main/java/cn/edu/jenny/niniweather/nini_util/Utility.java
+++ app/src/main/java/cn/edu/jenny/niniweather/nini_util/Utility.java
@@ -2,51 +2,69 @@ package cn.edu.jenny.niniweather.nini_util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import cn.edu.jenny.niniweather.nini_db.City;
 import cn.edu.jenny.niniweather.nini_db.Province;
+import cn.edu.jenny.niniweather.nini_gson.Weather;
 
 public class Utility {
     //解析处理服务器返回的省级数据
-    public static boolean handProvinceResponse(String response){
-        if(!TextUtils.isEmpty(response)){
-            try{
-                JSONArray allProvinces=new JSONArray(response);
-                for(int i=0;i<allProvinces.length();i++){
-                    JSONObject provinceObject=allProvinces.getJSONObject(i);
-                    Province province=new Province();
+    public static boolean handProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0; i < allProvinces.length(); i++) {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
                     province.setProvinceName(provinceObject.getString("name"));
                     province.setProvinceCode(provinceObject.getInt("id"));
                     province.save();
                 }
                 return true;
-            }catch(JSONException e){
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
         return false;
     }
 
-    public static boolean handCityResponse(String response,int provinceId){
-        if(!TextUtils.isEmpty(response)){
-            try{
-                JSONArray allCities=new JSONArray(response);
-                for(int i=0;i<allCities.length();i++){
-                    JSONObject cityObject=allCities.getJSONObject(i);
-                    City city=new City();
+    public static boolean handCityResponse(String response, int provinceId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0; i < allCities.length(); i++) {
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
                     city.setCityName(cityObject.getString("name"));
                     city.setCityCode(cityObject.getInt("id"));
                     city.setProvinceId(provinceId);
                     city.save();
                 }
                 return true;
-            }catch(JSONException e){
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
         return false;
+    }
+
+
+//将返回的JSON数据解析成weather实体类
+
+    public static Weather handledWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
